947349dd3633791015670c5593fb21348ef85433
Heqing Ren <heqing.ren@uzh.ch>
2023-04-01 11:40:08 +0200

    delete the content of all test classes due to dismatch with the main folder

--- src/test/java/ch/uzh/ifi/hase/soprafs23/repository/UserRepositoryIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/repository/UserRepositoryIntegrationTest.java
@@ -13,28 +13,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @DataJpaTest
 public class UserRepositoryIntegrationTest {
 
-  @Autowired
-  private TestEntityManager entityManager;
 
-  @Autowired
-  private UserRepository userRepository;
 
-  @Test
-  public void findByName_success() {
-    // given
-    User user = new User();
-    user.setUsername("firstname@lastname");
-    user.setToken("1");
-
-    entityManager.persist(user);
-    entityManager.flush();
-
-    // when
-    User found = userRepository.findByUsername(user.getUsername());
-
-    // then
-    assertNotNull(found.getId());
-    assertEquals(found.getUsername(), user.getUsername());
-    assertEquals(found.getToken(), user.getToken());
-  }
 }
--- src/test/java/ch/uzh/ifi/hase/soprafs23/rest/mapper/DTOMapperTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/rest/mapper/DTOMapperTest.java
@@ -14,31 +14,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * works.
  */
 public class DTOMapperTest {
-  @Test
-  public void testCreateUser_fromUserPostDTO_toUser_success() {
-    // create UserPostDTO
-    UserPostDTO userPostDTO = new UserPostDTO();
-    userPostDTO.setUsername("username");
-
-    // MAP -> Create user
-    User user = DTOMapper.INSTANCE.convertUserPostDTOtoEntity(userPostDTO);
-
-    // check content
-    assertEquals(userPostDTO.getUsername(), user.getUsername());
-  }
-
-  @Test
-  public void testGetUser_fromUser_toUserGetDTO_success() {
-    // create User
-    User user = new User();
-    user.setUsername("firstname@lastname");
-    user.setToken("1");
-
-    // MAP -> Create UserGetDTO
-    UserGetDTO userGetDTO = DTOMapper.INSTANCE.convertEntityToUserGetDTO(user);
-
-    // check content
-    assertEquals(user.getId(), userGetDTO.getId());
-    assertEquals(user.getUsername(), userGetDTO.getUsername());
-  }
+  
+  
 }
