0cc51b5247e5d88e49fcff89f1c27a18903d917d
ruiruiwang <wangruirui45@outlook.com>
2023-04-08 21:42:31 +0200

    enable uploading images

--- src/test/java/ch/uzh/ifi/hase/soprafs23/repository/UserRepositoryIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/repository/UserRepositoryIntegrationTest.java
@@ -6,6 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Blob;
+import java.sql.SQLException;
+import java.util.Base64;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -19,13 +25,14 @@ public class UserRepositoryIntegrationTest {
     private UserRepository userRepository;
 
     @Test
-    public void findByUsername_success() {
+    public void findByUsername_success() throws IOException, SQLException {
         // given
         User user = new User();
         user.setUsername("firstname@lastname");
         user.setPassword("1111");
         user.setToken("1");
-
+        InputStream inputStream = getClass().getResourceAsStream("/default.png");
+        user.setImage(Base64.getEncoder().encodeToString(inputStream.readAllBytes()));
         entityManager.persist(user);
         entityManager.flush();
 
