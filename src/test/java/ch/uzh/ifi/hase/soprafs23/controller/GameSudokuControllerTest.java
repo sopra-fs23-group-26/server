8f4dc93b7dc559e04bf450f8f416e265bd1a6f54
luojiewen <979822485@qq.com>
2023-05-15 01:43:03 +0200

    add tests for GameSudokuController

--- src/test/java/ch/uzh/ifi/hase/soprafs23/controller/GameSudokuControllerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/controller/GameSudokuControllerTest.java
@@ -1,25 +1,81 @@
 package ch.uzh.ifi.hase.soprafs23.controller;
 
+import ch.uzh.ifi.hase.soprafs23.entity.User;
+import ch.uzh.ifi.hase.soprafs23.repository.GameHistoryRepository;
+import ch.uzh.ifi.hase.soprafs23.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
-@ExtendWith(MockitoExtension.class)
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(GameSudokuController.class)
 class GameSudokuControllerTest {
 
     @InjectMocks
     private GameSudokuController gameSudokuController;
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @MockBean
+    private GameHistoryRepository gameHistoryRepository;
+
     @Test
-    public void testGetQuestion() {
-//        String[][] question = gameSudokuController.getQuestion("Hard");
-//
-//        // Check that the question has a 9x9 dimension
-//        Assertions.assertEquals(9, question.length);
-//        for (int i = 0; i < 9; i++) {
-//            Assertions.assertEquals(9, question[i].length);
-//        }
+    public void testGetQuestion() throws Exception {
+
+        MockHttpServletRequestBuilder request = get("/sudoku/create/Medium")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(request)
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testValidateAnswer_ValidAnswer() throws Exception {
+
+        User user = new User();
+        user.setUsername("user");
+
+        when(userService.getUserById(anyLong())).thenReturn(user);
+
+        String[][] answer = {{"5", "3", ".", ".", "7", ".", ".", ".", "."},
+                {"6", ".", ".", "1", "9", "5", ".", ".", "."},
+                {".", "9", "8", ".", ".", ".", ".", "6", "."},
+                {"8", ".", ".", ".", "6", ".", ".", ".", "3"},
+                {"4", ".", ".", "8", ".", "3", ".", ".", "1"},
+                {"7", ".", ".", ".", "2", ".", ".", ".", "6"},
+                {".", "6", ".", ".", ".", ".", "2", "8", "."},
+                {".", ".", ".", "4", "1", "9", ".", ".", "5"},
+                {".", ".", ".", ".", "8", ".", ".", "7", "9"}};
+
+        // 构造PUT请求
+        MockHttpServletRequestBuilder requestBuilder = put("/sudoku/validation/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(answer));
+
+        // 发送请求，并验证响应结果
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk());
+
     }
 }
